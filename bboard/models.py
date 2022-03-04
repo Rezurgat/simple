3ed4917aa4af121620359de2fd5a118b623ffa6e
Rezurgat <Killa.S.V.ru@bk.ru>
2022-03-04 19:40:21 +0300

    Create admin website. Work with register.Create a new model 'Rubric'

--- bboard/models.py
+++ bboard/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
 class Bb(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT,verbose_name='Рубрика' )
+    title = models.CharField(max_length=100, verbose_name='Наименование товара')
+    content = models.TextField(null=True, blank=True, verbose_name='Описание')
+    price = models.FloatField(null=True, blank=True, verbose_name='Цена')
+    published = models.DateTimeField(auto_now_add=True, db_index=True, verbose_name='Дата публикации')
+
+    class Meta:
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+        ordering = ['published']
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Рубрика'
+        verbose_name_plural = 'Рубрики'
+        ordering = ['name']
+
+
